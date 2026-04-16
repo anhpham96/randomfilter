c6c9046eef67279f66a49d5600695c7e756d4d65
Anh Pham <anh.pham@vogcalgaryappdeveloper.com>
2026-04-16 16:31:54 +0700

    Setup Project

---
+++ RandomFilter/RandomFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomFilterApp.swift
+//  RandomFilter
+//
+//  Created by Pham Nguyen Nhat Anh on 16/4/26.
+//
+
+import SwiftUI
+
+@main
+struct RandomFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RandomFilterTests/RandomFilterTests.swift
@@ -0,0 +1,19 @@
+//
+//  RandomFilterTests.swift
+//  RandomFilterTests
+//
+//  Created by Pham Nguyen Nhat Anh on 16/4/26.
+//
+
+import Testing
+@testable import RandomFilter
+
+struct RandomFilterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
